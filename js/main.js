0ad60e3568d6cf150352cd20fda1d12800af9e2b
Voldemarrs <vovattn123@gmail.com>
2023-09-09 12:44:45 +0300

    asd

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const inputOne = document.querySelector('.inputOne')
+const inputTwo = document.querySelector('.inputTwo')
+const plusBtn = document.querySelector('.plus')
+const minusBtn = document.querySelector('.minus')
+const umnBtn = document.querySelector('.umn')
+const delBtn = document.querySelector('.del')
+const submitBtn = document.querySelector('.submitBtn')
+const resultEl = document.querySelector('.result')
+
+let action = '+'
+
+plusBtn.onclick = function(){
+    action = '+'
+}
+
+minusBtn.onclick = function(){
+    action = '-'
+}
+
+umnBtn.onclick = function(){
+    action = '*'
+}
+
+delBtn.onclick = function(){
+    action = '/'
+}
+
+submitBtn.onclick = function(){
+    if (action === '+'){
+        const sum = Number(inputOne.value) + Number(inputTwo.value)
+        resultEl.textContent = sum    
+    } else if(action === '-'){
+        const sum = Number(inputOne.value) - Number(inputTwo.value)
+        resultEl.textContent = sum    
+    } else if(action === '*'){
+        const sum = Number(inputOne.value) * Number(inputTwo.value)
+        resultEl.textContent = sum    
+    } else if(action === '/'){
+        const sum = Number(inputOne.value) / Number(inputTwo.value)
+        resultEl.textContent = sum    
+    }
+    }
+
+    
